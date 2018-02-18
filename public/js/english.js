6ad0137558bb679f10cdc39d80d51d26e932bf0c
a-4k <36568538+a-4k@users.noreply.github.com>
2018-02-18 13:53:59 -0500

    Added Functions

--- public/js/english.js
+++ public/js/english.js
@@ -3,22 +3,30 @@ data:{},
 
     read: function(source)
     {
-        console.log(source);
+        //console.log(source);
         var linArray = source.split("\n");
+        bool = true;
+        while(bool){
         for(lin = 0; lin < linArray.length; lin++){
         var srcArray = linArray[lin].split(" ");
         for(i = 0; i < srcArray.length; i++){
+            
             if(srcArray[i] == "variable"){
-            English.data[srcArray[i+1]] = srcArray[i+3];   
-            console.log(srcArray[i+1]);
+                if(English.data[srcArray[i+1]] != null){
+                    console.log("Error: Variable declared twice");
+                    bool = false;
+                }
+                  else
+                 English.data[srcArray[i+1]] = srcArray[i+3];   
              }
             if(srcArray[i]==("equals") && srcArray[i-2] != "variable"){
-                console.log('y');
+                //console.log('y');
                 English.data[srcArray[i-1]] = srcArray[i+1];
+                    }
                 }
             }
         }
     }
 }
-English.read("variable x equals 3 \n x equals 5");
+English.read("variable x equals 3");
 console.log(English.data.x);
