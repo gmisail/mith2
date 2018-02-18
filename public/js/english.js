a655c20bd03ecbef242c39cc4e5f841e52c1f7fe
a-4k <36568538+a-4k@users.noreply.github.com>
2018-02-18 13:56:23 -0500

    functions v2

--- public/js/english.js
+++ public/js/english.js
@@ -1,26 +1,43 @@
 var English = {
-data:{},
+data:{}, functions:{},
 
     read: function(source)
     {
-        //console.log(source);
         var linArray = source.split("\n");
         bool = true;
+        functionBuffer = [];
+        functionBufferNum = 0;
+        for(f = 0; f < linArray.length; f++){
+            if(linArray[f] = "function"){
+                boolB = true
+                funWord = 3;
+                while(boolB){
+                    if((linArray[funWord] + linArray[f+1]) == ("end" + linArray[f+1])){
+                        functions[linArray[f+1]] = functionBuffer;
+                        functionBuffer = [];
+                        functionBufferNum = 0;
+                        boolB = false;
+                    }
+                    else {
+                        functionBuffer[functionBufferNum] = linArray[funWord];
+                        functionBufferNum++;
+                    }
+                }
+            }
+        }
         while(bool){
         for(lin = 0; lin < linArray.length; lin++){
         var srcArray = linArray[lin].split(" ");
         for(i = 0; i < srcArray.length; i++){
-            
             if(srcArray[i] == "variable"){
-                if(English.data[srcArray[i+1]] != null){
-                    console.log("Error: Variable declared twice");
-                    bool = false;
-                }
-                  else
-                 English.data[srcArray[i+1]] = srcArray[i+3];   
+               // if(English.data[srcArray[i+1]] != null){
+               //   console.log("Error: Variable declared twice");
+               // bool = false;
+               //}
+               //else
+                English.data[srcArray[i+1]] = srcArray[i+3];   
              }
             if(srcArray[i]==("equals") && srcArray[i-2] != "variable"){
-                //console.log('y');
                 English.data[srcArray[i-1]] = srcArray[i+1];
                     }
                 }
@@ -30,3 +47,4 @@ data:{},
 }
 English.read("variable x equals 3");
 console.log(English.data.x);
+//comment
